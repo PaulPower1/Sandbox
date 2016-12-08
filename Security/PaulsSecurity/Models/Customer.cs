a06bc28e6833a9c4d0aa7d338710ff8623bb81b8
PaulPower1 <paul.power@quest.com>
2016-12-08 13:24:34 +1100

    Add in search control and customer db records which can be accessed once the user is authorized

---
+++ Security/PaulsSecurity/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace PaulsSecurity.Models
+{
+    public class Customer
+    {
+        public string File_As { get; set; }
+        public string ContactName { get; set; }
+        public int ID { get; set; }
+        public string Company { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+        public string JobTitle { get; set; }
+        public string BusinessPhone { get; set; }
+        public string HomePhone { get; set; }
+        public string MobilePhone { get; set; }
+    }
+}
